dd52d65b9f708cc2ab3a5c6652e18583dadc9d90
yemyat89 <yemyatthein@gmail.com>
2014-03-01 00:08:40 +0100

    goNext() method implemented

--- GithubTest/src/HelloWorld.java
+++ GithubTest/src/HelloWorld.java
@@ -2,8 +2,9 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		goNext();
 	}
 	public static void goNext() {
-		
+		System.out.println("Go where??");
 	}
 }
